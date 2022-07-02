6bb3ad0e6938bda34c8d2ae5e8069e6cbf9ac360
brunogoo <43398800+brunogoo@users.noreply.github.com>
2022-07-02 12:52:15 -0400

    Delete homework7 directory

--- homework7/AvgGPA.java
+++
@@ -1,9 +0,0 @@
-// AvgGPA.java: Reads from standard input a list of letter grades and computes
-// and prints the average GPA (the average of the numbers corresponding to
-// the grades).
-
-public class AvgGPA {
-    public static void main(String[] args) {
-        ...
-    }
-}
