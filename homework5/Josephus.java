ea680d1186cc7f64485ce9dffc3fe8a7fdf21b13
brunogoo <43398800+brunogoo@users.noreply.github.com>
2022-07-02 12:51:37 -0400

    Delete homework5 directory

--- homework5/Josephus.java
+++
@@ -1,9 +0,0 @@
-// Josephus.java: Takes N and M from the command line and prints out the order 
-// in which people are eliminated (and thus would show Josephus where to sit in 
-// the circle).
-
-public class Josephus {
-    public static void main(String[] args) {
-        ...
-    }
-}
